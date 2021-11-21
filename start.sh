8d7be04b0fab59a55e178e26be7910096c5b5fee
Moeinovic <76170229+moeinovic@users.noreply.github.com>
2021-11-21 13:26:56 +0330

    Create start.sh

---
+++ start.sh
@@ -0,0 +1,4 @@
+while true
+do
+  ffmpeg -re -i "https://tv4.live/api/stream/moe.inovic.z@gmail.com/123456789/livetv.epg/eleven.sports.1.pl.m3u8" -user_agent "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:93.0) Gecko/20100101 Firefox/93.0" -c:v copy -c:a aac -f flv "rtmp://rtmp.lahzecdn.com:443/pro/aumermfynd"
+done
