e7a4655058c9759e0b24dad7958b9843cbf5147f
Moeinovic <76170229+moeinovic@users.noreply.github.com>
2021-11-21 14:30:05 +0330

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,4 +1,1 @@
-while true
-do
-  ffmpeg -re -i "https://tv4.live/api/stream/moe.inovic.z@gmail.com/123456789/livetv.epg/eleven.sports.1.pl.m3u8" -user_agent "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:93.0) Gecko/20100101 Firefox/93.0" -c:v copy -c:a aac -f flv "rtmp://rtmp.lahzecdn.com:443/pro/aumermfynd"
-done
+ffmpeg -re -i "https://tv4.live/api/stream/moe.inovic.z@gmail.com/123456789/livetv.epg/eleven.sports.1.pl.m3u8" -user_agent "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:93.0) Gecko/20100101 Firefox/93.0" -c:v copy -c:a aac -f flv "rtmp://rtmp.cdn.asset.aparat.com:443/event/e4bc3605a20cfdc7a1e206bb2084fac3b?s=aaf9bf799b6d45a4"
