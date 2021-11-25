e943d83c6cfe84025db604c5e4226234460dc7f2
Moeinovic <76170229+moeinovic@users.noreply.github.com>
2021-11-25 18:47:35 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,1 +1,1 @@
-ffmpeg -i "http://electrotv.us:25461/live/momo4545/momo4545/1123899.m3u8" -c:v copy -c:a copy -f flv "rtmp://5.106.13.131/shadstream/619fbf4093900c4ad4737b1bwmggjbdayzymwpmwfqxxycamhshcgmji?pt=fmffbyyofznbtzvugpuvnjqhsxnlhvbm"
+ffmpeg -i "http://electrotv.us:25461/live/momo4545/momo4545/1123899.m3u8" -c:v copy -c:a copy -f flv "rtmp://rtmp.lahzecdn.com:443/pro/vnmhmblbvn"
