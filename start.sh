1c1c4e4e4a62da051111236b7dedf27ff3c6a72a
Moeinovic <76170229+moeinovic@users.noreply.github.com>
2021-11-26 10:13:11 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,1 +1,1 @@
-ffmpeg -i "https://tv4.live/api/stream/moeino.vi.c.z@gmail.com/123456789/livetv.epg/match.tv.ru.m3u8" -c:v copy -c:a copy -f flv "rtmp://rtmp.lahzecdn.com:443/pro/vnmhmblbvn"
+ffmpeg -i "https://tv4.live/api/stream/moeino.vi.c.z@gmail.com/123456789/livetv.epg/match.tv.ru.m3u8" -c:v copy -c:a copy -f flv "rtmp://rtmp.lahzecdn.com:1935/pro/vnmhmblbvn"
