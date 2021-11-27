445a318e63e065b792884827bdb9205038a906a8
Moeinovic <76170229+moeinovic@users.noreply.github.com>
2021-11-27 18:36:00 +0330

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,1 +1,1 @@
-ffmpeg -i "https://tv4.live/api/stream/moeino.vi.c.z@gmail.com/123456789/livetv.epg/bt.sport.1.uk.m3u8" -vcodec copy -acodec aac -f flv "rtmp://push.ir-thr-mn-cluster.arvanlive.com:1935/lP80pm5WVD/X8ZqQV7G3n"
+ffmpeg -i "https://tv4.live/api/stream/moeino.vi.c.z@gmail.com/123456789/livetv.epg/bt.sport.1.uk.m3u8" -vcodec copy -acodec aac -f flv "rtmp://stream.dlive.tv/live/2JmKm1hEgWLU1fzLEaZcQm09zlVqZ732_moeinovic1"
