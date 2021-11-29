0c02555de42c84cad52866e5fcd43e6d548008b6
Moeinovic <76170229+moeinovic@users.noreply.github.com>
2021-11-30 00:33:01 +0330

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,1 +1,1 @@
-ffmpeg -i "https://tv4.live/api/stream/moeino.vi.c.z@gmail.com/123456789/livetv.epg/bt.sport.1.uk.m3u8" -c:v copy -c:a copy -bsf:a aac_adtstoasc -f flv "rtmp://ingest-us-phoenix.brime.tv/live/QTVfrqwqeOvJima6JIwj?auth=dW8Cecss73I5mk5WeGCLXlojseRZnKAT6kLzRpaG"
+ffmpeg -i "https://tv4.live/api/stream/alexwhel7@yahoo.com/rBE2dxcHAqRa6iv/livetv.epg/eleven.sports.1.pt.m3u8" -c:v copy -c:a aac -f flv "rtmp://live.us.picarto.tv/golive/ptv_1234965_6f92ae4270b5"
