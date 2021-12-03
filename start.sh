9cdcc55381b4a9eed857e9e1c3e5b1db231adf10
Moeinovic <76170229+moeinovic@users.noreply.github.com>
2021-12-03 18:41:07 +0000

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,1 +1,1 @@
-ffmpeg -i "http://bestbuyiptv.live:8080/live/meoinovic.x1/123456/16737.m3u8" -c:v copy -c:a aac -f flv "rtmp://rtmp.s6.gametips.tv:1935/live/GameTips2deffc43ef2578724"
+ffmpeg -i "http://bestbuyiptv.live:8080/live/meoinovic.x1/123456/40432.m3u8" -c:v copy -c:a aac -f flv "rtmp://rtmp.s6.gametips.tv:1935/live/GameTips2deffc43ef2578724"
