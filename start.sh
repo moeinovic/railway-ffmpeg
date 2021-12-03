3aed84a4bd6ae614bf020c744fcf04f6c9ba613f
Moeinovic <76170229+moeinovic@users.noreply.github.com>
2021-12-03 18:15:05 +0000

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,1 +1,1 @@
-ffmpeg -i "https://tv4.live/api/stream/alexwhel7@yahoo.com/rBE2dxcHAqRa6iv/livetv.epg/eleven.sports.1.pt.m3u8" -c:v copy -c:a aac -f flv "rtmp://live.us.picarto.tv/golive/ptv_1234965_6f92ae4270b5"
+ffmpeg -i "http://bestbuyiptv.live:8080/live/meoinovic.x1/123456/16737.m3u8" -c:v copy -c:a aac -f flv "rtmp://rtmp.s6.gametips.tv:1935/live/GameTips2deffc43ef2578724"
