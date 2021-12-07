22a95f306f3e1fb2775557bd289f8646d29dbf56
Moeinovic <76170229+moeinovic@users.noreply.github.com>
2021-12-07 14:08:36 +0330

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
 while true
 do
-ffmpeg -i "https://tv4.live/api/stream/moeinovic.a@yahoo.com/123456789/livetv.epg/eleven.sports.2.pl.m3u8" -c:v copy -c:a aac -f flv "rtmp://rtmp.lahzecdn.com:443/pro/aumermfynd"
+ffmpeg -i "https://tv4.live/api/stream/moeinovic.a@yahoo.com/123456789/livetv.epg/eleven.sports.1.pl.m3u8"  -filter:v fps=30 -c:v libx264 -x264-params keyint=120:scenecut=0 -b:v 4M -maxrate 4M -bufsize 2M -c:a copy -f flv "rtmp://rtmp.cdn.asset.aparat.com:443/event/e4bc3605a20cfdc7a1e206bb2084fac3b?s=065ee80a4f63e8e9"
 done
